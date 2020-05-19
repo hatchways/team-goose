a230751aaa99337da5b654041667dd3d0641aabd
h0vscat <aarontsa2@gmail.com>
2020-05-19 13:23:48 -0400

    Revert "set up database"
    This reverts commit e57edcc6cf521ee4db6df1711c9208f89d56190d.

--- server/db/mongoose.js
+++
@@ -1,5 +0,0 @@
-
-const mongoose = require('mongoose');
-const url = process.env.MONGOLAB_URI;
-mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-module.exports = { mongoose }
