c114e6ce60daec78fae30522fcddf48aab2a8a2e
ManpreetBhuller <40129775+ManpreetBhuller@users.noreply.github.com>
2020-05-21 17:49:10 -0400

    Backend-APIs-SignUp-Login

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const user = require('./routes/api/user');
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
 
@@ -21,6 +22,7 @@ app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
+app.use('/api/user', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
