2116e5ae3e6981cbfb7211b32fb267409db0ee26
h0vscat <aarontsa2@gmail.com>
2020-05-27 17:48:14 -0400

    seperate routes

--- server/routes/matches.js
+++ server/routes/matches.js
@@ -1,30 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const MatchManager = require('../game_manager/MatchManager');
+const MatchManager = require("../game_manager/MatchManager");
 
 router.post("/match", (req, res) => {
-    const hostId = req.body;
-    try {
-        const matchId = MatchManager.createMatch(hostId);
-        res.json(matchId);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-  });
-
-router.post("/match/:id/join-match", (req, res) => 
-{
-    const matchId = req.params.id;
-    console.log(matchId, "req params")
-    try {
-        const message = MatchManager.joinMatch(matchId);
-        res.json(message);
-    } catch (err) {
-        console.err(err.message);
-        res.status(500).send("Server error");
-    }
-})
+  const hostId = req.body;
+  try {
+    const matchId = MatchManager.createMatch(hostId);
+    res.json(matchId);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+router.post("/match/:id/join-match", (req, res) => {
+  const matchId = req.params.id;
+  try {
+    const message = MatchManager.joinMatch(matchId);
+    res.json(message);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
