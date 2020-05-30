5aee95500b3d3c19dcbadd9020a6bbbadb32af41
Tony Chen <tonyc856@users.noreply.github.com>
2020-05-29 22:17:48 -0700

    Added more chat styling. Restructured chat log to properly display messages. Chat log now automatically scroll towards the bottom for new messages.

--- client/src/pages/chat/Chat.js
+++ client/src/pages/chat/Chat.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
-import { Button, Container, Grid, TextField, Box } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { animateScroll } from "react-scroll";
+import { Button, Grid, TextField, Box } from "@material-ui/core";
 
 import Dialog from "./Dialog";
 import "./Chat.css";
 
+const CHAT_LOG_ELEMENT_ID = "chat-log";
+
 function Chat() {
   const [inputText, setInputText] = useState("");
   const [log, setLog] = useState([]);
+
+  useEffect(() => {
+    // listen to messages being broadcasted from backend's socket
+    // message: {from: "Bonnie", text: "Let's pick dog!"};
+    // setLog([...log, message]);
+  }, []);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [log]);
 
   const onInputTextChange = (event) => {
     const value = event.target.value;
@@ -15,40 +28,40 @@ function Chat() {
 
   const sendMessage = (event, inputText) => {
     event.preventDefault();
-    const newLog = [...log, inputText];
+    const message = { from: "Tony", text: inputText };
+    const newLog = [...log, message];
     setLog(newLog);
     setInputText("");
   };
 
+  const scrollToBottom = () => {
+    animateScroll.scrollToBottom({
+      containerId: CHAT_LOG_ELEMENT_ID,
+    });
+  };
+
   const generateChatLog = () => {
     return log.map((message, index) => {
-      return (
-        <Grid item key={index}>
-          <Dialog message={message} />
-        </Grid>
-      );
+      return <Dialog key={index} from={message.from} text={message.text} />;
     });
   };
 
   return (
     <div id="chat">
-      <Box component="div" className="chat-log">
-        <Grid
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="flex-end"
-          spacing={2}
-        >
-          {log.length > 0 ? generateChatLog() : null}
-        </Grid>
+      <Box component="div" id={`${CHAT_LOG_ELEMENT_ID}`}>
+        {log.length > 0 ? generateChatLog() : null}
       </Box>
       <form
         onSubmit={(event) => {
           sendMessage(event, inputText);
         }}
       >
-        <Grid container alignItems="center" spacing={1}>
+        <Grid
+          container
+          alignItems="center"
+          spacing={1}
+          className="chat-input-container"
+        >
           <Grid item xs>
             <TextField
               type="text"
--- client/src/pages/chat/Dialog.js
+++ client/src/pages/chat/Dialog.js
@@ -1,13 +1,49 @@
-import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Box, Grid, Typography } from "@material-ui/core";
 
 import "../common/common.css";
 
-function Dialog({ message }) {
+const DIRECTIONS = {
+  LEFT: "row",
+  RIGHT: "row-reverse",
+};
+
+const SENDER_COLOR = {
+  text: "message-out",
+  background: "bg-red", // TODO: will need to assign background color depending on player's team color
+};
+
+function Dialog({ from, text }) {
+  const [isSender] = useState(false);
+  const [styles, setStyles] = useState("");
+  const [direction, setDirection] = useState(DIRECTIONS.LEFT);
+
+  useEffect(() => {
+    let newStyles = "";
+    let newDirection = "";
+
+    if (isSender) {
+      newStyles = `${SENDER_COLOR.text} ${SENDER_COLOR.background}`;
+      newDirection = DIRECTIONS.RIGHT;
+    } else {
+      newDirection = DIRECTIONS.LEFT;
+    }
+
+    setDirection(newDirection);
+    setStyles(newStyles);
+  }, [isSender]);
+
   return (
-    <Box component="div" className="dialog sender bg-red">
-      <Typography variant="body1">{message}</Typography>
-    </Box>
+    <Grid container direction={`${direction}`} justify="flex-start" spacing={3}>
+      <Grid item className="message">
+        <Box component="div" className="sender">
+          <Typography variant="body2">{`${from}:`}</Typography>
+        </Box>
+        <Box component="div" className={`dialog ${styles}`}>
+          <Typography variant="body1">{text}</Typography>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
