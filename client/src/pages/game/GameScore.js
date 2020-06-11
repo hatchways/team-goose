50379ce8a2c87507d8a0ce7f117cd41b954fa72c
Tony Chen <tonyc856@users.noreply.github.com>
2020-06-11 12:28:09 -0700

    Remove used import.

--- client/src/pages/game/GameScore.js
+++ client/src/pages/game/GameScore.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
