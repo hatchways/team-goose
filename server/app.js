fe1a88bdaf8096e8da31a511b4510efdd3c51b6f
Tony Chen <tonyc856@users.noreply.github.com>
2020-06-12 09:43:25 -0700

    deploy fixes to client build path

--- server/app.js
+++ server/app.js
@@ -49,9 +49,9 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "../client", "build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
 });
 
 module.exports = app;
