53ddb62ec954d61c3fdd633cfb160b576d75f55e
Tony Chen <tonyc856@users.noreply.github.com>
2020-06-12 13:16:53 -0700

    Deploy fixes.

--- server/app.js
+++ server/app.js
@@ -21,9 +21,9 @@ app.use("/api/user", user);
 //app.use("/api/match", match);
 
 // catch 404 and forward to error handler
-//app.use(function (req, res, next) {
-//  next(createError(404));
-//});
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
